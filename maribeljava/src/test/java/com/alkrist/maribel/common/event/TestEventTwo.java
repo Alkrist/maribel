7c2b37cb8003f98eb44de8ec29152bae247b7862
Mikhail Gerasimov <xgerasim@fi.muni.cz>
2022-04-23 14:29:44 +0200

    added unit tests for events

---
+++ maribeljava/src/test/java/com/alkrist/maribel/common/event/TestEventTwo.java
@@ -0,0 +1,20 @@
+package com.alkrist.maribel.common.event;
+
+public class TestEventTwo extends Event{
+
+	private static final HandlerList handlers = new HandlerList();
+	
+	public String getMessage() {
+		return this.getClass().getCanonicalName();
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+}
